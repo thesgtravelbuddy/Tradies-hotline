3901dfca57e8b7f7ab51dfcafec667f9ec437caf
thesgtravelbuddy <the.sg.travel.buddy@gmail.com>
2026-09-02 00:38:02 +0800

    Add early Vercel logs

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import { createRequestHandler } from '../src/server.js';
 
+console.log('Vercel function loading started...');
+
 export default async function (request, response) {
+  console.log('Vercel function invoked.');
   return createRequestHandler({ request, response });
 }
